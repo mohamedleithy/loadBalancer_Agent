25345c97ce743c6208cd27208edf0c2e1562b2d9
mohamedleithy <leithy@aucegypt.edu>
2022-11-07 14:11:01 +0200

    implementing agent

--- src/main.rs
+++ src/main.rs
@@ -6,26 +6,59 @@ use std::thread;
 use std::time::Duration;
 use rand::Rng;
 
+use std::io::{prelude::*, BufReader};
+use std::path::Path;
+
+
+// fn loadIpAddresses(filename: impl AsRef<Path>) -> Vec<String> {
+//     let file = File::open(filename).expect("no such file");
+//     let buf = BufReader::new(file);
+//     buf.lines()
+//         .map(|l| l.expect("Could not parse line"))
+//         .collect()
+// }
+
+
 fn main() -> std::io::Result<()>{
     {
+        
+       
+
+       
 
         // thread to initiate requests to servers, upon recieving a request from clients 
+        // Reserve port 2020 for agent on the machine
         let handler = thread::spawn(move || {
-        let socket = UdpSocket::bind("192.168.1.3:5966").unwrap();
-
-            loop {
-                println!("Sending message to other servers");
-                let mut rng = rand::thread_rng();
-                let n = rng.gen_range(0, 3);
-                
-                
-                println!("Random number: {n}");
-                
             
-                 socket.send_to(b"Request recieved from: ", "192.168.1.3:5960").unwrap();
-  
+        // Load Ip Addresses of Servers from configuration file 
+        // The random generator is to randomize the starting node of each agent as not to initiate load from all agents to the first server
+        // ex: Agent 1: 0 1 2 Agent 2: 2 0 1 Agent 3: 1 2 0 
 
-               thread::sleep(Duration::from_millis(1000));
+            // let ipAddresses= loadIpAddresses("/home/g02-f22/Desktop/loadBalancer_Agent/config.txt");
+            // let mut rng = rand::thread_rng();
+            // let mut n = rng.gen_range(0, ipAddresses.len());
+            let clientToAgentMsg = "ClienToAgentMsg::";
+
+        let socket = UdpSocket::bind("10.40.44.255:2020").unwrap();
+            loop {
+
+                let mut buf = [0; 30]; // buffer for recieving 
+
+                
+                // println!("Random number: {n}");
+
+                // recieve from client 
+                let (amt, src) = socket.recv_from(&mut buf).unwrap();
+
+                thread::sleep(std::time::Duration::from_millis(1000));
+                
+                // send to servers in a round robin fashion 
+                socket.send_to(&buf, "10.40.44.255:2023").unwrap();
+                println!("{} Forwarded message from client to server", clientToAgentMsg);
+
+                // move to next in order 
+                //  n += 1;
+                //  n = n%ipAddresses.len(); 
     
             }
             
@@ -33,29 +66,33 @@ fn main() -> std::io::Result<()>{
 
             // thread to receive replies from servers, and sending back to client 
 
+            // receive on send port + 1 (2021)
+
             let handler1 = thread::spawn(move || {
-                let socket = UdpSocket::bind("192.168.1.3:5960").unwrap();
-        
+                let socket = UdpSocket::bind("10.40.44.255:2021").unwrap();
+                let agentToClientMsg = "AgentToClientMsg::";  
                 loop {
-                        println!("Recieving messages from other servers");
+                        
                         let mut buf = [0; 30]; // buffer for recieving 
 
                         // blocked till Recieving a message from any of the other servers 
 
+                        // recieve from server 
                         let (amt, src) = socket.recv_from(&mut buf).unwrap();
+                        println!("{} Message Recieved!", agentToClientMsg);
 
 
+                        // TODO 
 
-                        println!("Message Recieved!");
+                        /*Requires engineering how to get the ip address of the client*/
 
-                        println!("From: {:?}", src);
+                        socket.send_to(&buf, "10.40.44.255:2022").unwrap();
+
+
+                        println!("{}From: {:?}", agentToClientMsg, src);
                         //print the received data as a string 
 
                         println!("Message: {}", String::from_utf8_lossy(&buf));
-
-
-
-                        thread::sleep(Duration::from_millis(1000));
                  
                     }
                         
@@ -63,10 +100,10 @@ fn main() -> std::io::Result<()>{
 
 
 
-
         handler.join().unwrap();
         handler1.join().unwrap();
     }
+
 
     Ok(())
     
