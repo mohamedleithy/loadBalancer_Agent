0f3df21364461f3592ab6a38032075528c76dd93
mohamedleithy <leithy@aucegypt.edu>
2022-11-28 13:37:18 +0200

    operating agent

--- src/main.rs
+++ src/main.rs
@@ -36,7 +36,7 @@ fn main() -> std::io::Result<()>{
 
         // maintaing server info to send to servers and mark down servers 
 
-        let temp_servers: [server; 3] = [server { ip: ip.to_string(), state: true, temperature: 100}, server { ip: "172.20.10.4".to_string(), state: true, temperature: 100 }, server { ip: "172.20.10.3".to_string(), state: true, temperature: 100},];
+        let temp_servers: [server; 3] = [server { ip: ip.to_string(), state: true, temperature: 100}, server { ip: "192.168.8.106".to_string(), state: true, temperature: 100 }, server { ip: "192.168.8.106".to_string(), state: true, temperature: 100},];
         
         let server_info = Arc::new(Mutex::new(temp_servers));
         // thread to initiate requests to servers, upon recieving a request from clients 
@@ -44,14 +44,15 @@ fn main() -> std::io::Result<()>{
 
 
     let server_info_1 = Arc::clone(&server_info);
+
         let handler = thread::spawn(move || {
         // Load Ip Addresses of Servers from configuration file 
         // The random generator is to randomize the starting node of each agent as not to initiate load from all agents to the first server
         // ex: Agent 1: 0 1 2 Agent 2: 2 0 1 Agent 3: 1 2 0 
 
             // let ipAddresses= loadIpAddresses("/home/g02-f22/Desktop/loadBalancer_Agent/config.txt");
-            // let mut rng = rand::thread_rng();
-            // let mut n = rng.gen_range(0, ipAddresses.len());
+            let mut rng = rand::thread_rng();
+            let mut n = rng.gen_range(0, 3);
 
 
             let clientToAgentMsg = "ClienToAgentMsg::";
@@ -62,12 +63,17 @@ fn main() -> std::io::Result<()>{
         let mut server_info_11 = server_info_1.lock().unwrap();
             // inform all servers that this agent is up. 
             // TODO: LOOP ON ALL SERVERS, use for loop 
+            for server in server_info_11.iter_mut(){
 
-            socket.send_to(b"1", ip.to_string() + ":2030").unwrap(); 
-            std::mem::drop(server_info_11); 
+                let adr = format!("{}{}", server.ip , ":2030"); 
+
+                socket.send_to(b"1",  adr).unwrap();
+            }
+             
+            std::mem::drop(server_info_11);
             
             loop {
-
+                
                 let mut buf = [0; 30]; // buffer for recieving 
 
                 
@@ -76,7 +82,7 @@ fn main() -> std::io::Result<()>{
                 // recieve from client 
                 let (_, src) = socket.recv_from(&mut buf).unwrap();
 
-                thread::sleep(std::time::Duration::from_millis(1000));
+              
 
                 //appending the client ip to the message
                 let mut buf1 = src.to_string().into_bytes();
@@ -86,14 +92,22 @@ fn main() -> std::io::Result<()>{
                 // send to servers in a round robin fashion TODO: USE N 
 
                 // check if N is not down otherwise, send to N+1 
-
-
-                socket.send_to(&buf1, "192.168.1.3:2023").unwrap();
+                let mut server_info_11 = server_info_1.lock().unwrap();
+                while(!server_info_11[n].state){
+                    n += 1;
+                    n = n%server_info_11.len(); 
+                }
+                let adr = format!("{}{}",server_info_11[n].ip, ":2023"); 
+                
+                socket.send_to(&buf1, adr).unwrap();
                 println!("{} Forwarded message from client to server", clientToAgentMsg);
 
                 // move to next in order 
-                //  n += 1;
-                //  n = n%server_info_11.len(); 
+                n += 1;
+                n = n%server_info_11.len(); 
+
+                std::mem::drop(server_info_11);
+                 
             }
             
             });
@@ -126,7 +140,7 @@ fn main() -> std::io::Result<()>{
                         println!("client ip: {}", src1);
 
 
-                        thread::sleep(std::time::Duration::from_millis(1000));
+                       
                         //convert src1 To Socket Address
                         let src1 = src1.parse::<std::net::SocketAddr>().unwrap();
 
@@ -159,7 +173,7 @@ fn main() -> std::io::Result<()>{
                         let mut buf = [0; 60]; // buffer for recieving 
 
                         // blocked till Recieving a message from any of the other servers 
-                        let mut server_info_22 = server_info_2.lock().unwrap();
+                       
                         // recieve from server 
                         let (_, src) = socket.recv_from(&mut buf).unwrap();
 
@@ -171,13 +185,13 @@ fn main() -> std::io::Result<()>{
 
                         let mut server_info_22 = server_info_2.lock().unwrap();
 
-                        for server in serverInfo22.iter_mut(){
+                        for server in server_info_22.iter_mut(){
                             if src1.to_string() == (server.ip.to_string())  {  
 
-                                server_info_22.state = false; 
+                                server.state = false; 
                             } else{
 
-                                server_info_22.state = true; 
+                                server.state = true; 
                             }
 
                         }
@@ -198,7 +212,7 @@ fn main() -> std::io::Result<()>{
 
 
 // UPON TERMINATING INFORM SERVERS THAT THIS AGENT IS OFF 
-
+let server_info_3 = Arc::clone(&server_info);
 let handler3 = thread::spawn(move || {
     let socket = UdpSocket::bind(ip.to_string() +":4000").unwrap();
     let agentToServerMsg = "AgentToServerMsg::";  
@@ -212,9 +226,16 @@ let handler3 = thread::spawn(move || {
             let stdin = io::stdin(); // We get `Stdin` here.
             stdin.read_line(&mut user_input).unwrap();
         if user_input == "exit\n" {
-            
-           socket.send_to(b"0", ip.to_string() + ":2030").unwrap(); 
-           exit(0);
+            let mut server_info_33 = server_info_3.lock().unwrap();
+            for server in server_info_33.iter_mut(){
+
+                let adr = format!("{}{}", server.ip , ":2030"); 
+
+                socket.send_to(b"0",  adr).unwrap();
+            }
+           
+            std::mem::drop(server_info_33);
+            exit(0);
         }
             
 
